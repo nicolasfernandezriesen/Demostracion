311a20129725f97d26e6dffe230c24678f8d0489
Nicolas Fernandez Riesen <riesenfnicolas@gmail.com>
2022-10-21 17:36:27 -0300

    Caso 3

---
+++ WindowsLab/FrmCaso9.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace WindowsLab
+{
+    partial class FrmCaso9
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(294, 204);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(129, 53);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Empezar";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // FrmCaso9
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStart);
+            this.Name = "FrmCaso9";
+            this.Text = "FrmCaso9";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+    }
+}
---
+++ WindowsLab/FrmCaso9.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsLab
+{
+    public partial class FrmCaso9 : Form
+    {
+        string[] operators = new string[4];
+        decimal[] salary = new decimal[3];
+        decimal total = 0;
+        decimal salaryMax=0;
+        string operatorName;
+
+        public FrmCaso9()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            LoadOperators();
+
+            foreach (string name in operators)
+            {
+                LoadSalary(name);
+                ShowSalary(name);
+            }
+
+            MessageBox.Show("Total de sueldos pagados: " + total.ToString() + "Operador con mayor ingresos acumulados: " + operatorName);
+        }
+
+        #region My Methods
+
+        private void LoadOperators()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                operators[i] = Interaction.InputBox("Ingrese el nombre del operador " + (i + 1) + ": ");
+            }
+        }
+
+        private void LoadSalary(string name)
+        {
+            decimal aux = 0;
+
+            for (int i = 0; i < 3; i++)
+            {
+                salary[i] = Convert.ToDecimal(Interaction.InputBox("Ingrese el salario numero " + (i+1) + " del operador " + name));
+                aux = aux + salary[i];
+                total = total + salary[i];
+            }
+            if(aux > salaryMax)
+            {
+                salaryMax = aux;
+                operatorName = name;
+            }
+        }
+
+        private void ShowSalary(string name)
+        {
+            decimal aux = 0;
+
+            foreach (decimal num in salary)
+            {
+                aux = aux + num;
+            }
+
+            MessageBox.Show("Operador " + name + " salario total a pagarle: " + aux.ToString());
+        }
+
+        #endregion
+    }
+}
