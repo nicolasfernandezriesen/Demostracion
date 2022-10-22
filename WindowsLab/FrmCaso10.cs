85e60248e281a821c67699716a6ed00c9440f64f
Nicolas Fernandez Riesen <riesenfnicolas@gmail.com>
2022-10-22 14:14:18 -0300

    Caso 4 loops y vectores

---
+++ WindowsLab/FrmCaso10.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace WindowsLab
+{
+    public partial class FrmCaso10 : Form
+    {
+        string[] nombres;
+        int[] edades;
+        public FrmCaso10()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            nombres = new string[5];
+            edades = new int[5];
+
+            LoadArray();
+            ShowArrayAdults();
+        }
+
+        private void LoadArray()
+        {
+            for (int i = 0; i < 5; i++)
+            { 
+                nombres[i] = Interaction.InputBox("Ingrese el nombre de la persona","Nombre de la Persona");
+                edades[i] = Convert.ToInt32(Interaction.InputBox("Ingrese la edad de " + nombres[i], "Edad de la Persona"));
+            }
+        }
+
+        private void ShowArrayAdults()
+        {
+            string adults = "";
+            
+            for (int i = 0; i < 5; i++)
+            {
+                if (edades[i] >= 18)
+                {
+                    adults = adults + nombres[i] + " - ";
+                }
+            }
+
+            MessageBox.Show("Personas mayores de edad: " + adults);
+        }
+    }
+}
--- WindowsLab/Program.cs
+++ WindowsLab/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsLab
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FrmCaso9());
+            Application.Run(new FrmCaso10());
         }
     }
 }
